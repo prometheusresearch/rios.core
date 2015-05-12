d0bd987b9542191482257d6ac09dde07ed7af3a2
Jason Simeone <jason@prometheusresearch.com>
2015-05-12 10:01:32 -0400

    Added more output tests.

---
+++ tests/interaction_output_tests.py
@@ -0,0 +1,134 @@
+#
+# Copyright (c) 2015, Prometheus Research, LLC
+#
+
+
+from prismh.core.output import get_interaction_json
+
+
+INTERACTION = {
+    'defaultTimeout': {
+        'abort': {
+            'text': {
+                'en': 'Abort!',
+            },
+            'threshold': 123,
+        },
+        'warn': {
+            'threshold': 40,
+            'text': {
+                'fr': 'French warning',
+                'en': 'Warning!',
+            },
+        },
+    },
+    'steps': [
+        {
+            'options': {
+                'text': {
+                    'en': 'Introductory message',
+                },
+            },
+            'type': 'text',
+        },
+        {
+            'options': {
+                'error': {
+                    'en': 'You failed',
+                },
+                'enumerations': [
+                    {
+                        'text': {
+                            'en': 'Foo',
+                        },
+                        'id': 'foo',
+                    },
+                    {
+                        'id': 'bar',
+                        'text': {
+                            'en': 'Bar',
+                            'ar': 'Arabic Bar',
+                        },
+                    },
+                ],
+                'fieldId': 'field1',
+                'text': {
+                    'en': 'Text for field1',
+                },
+            },
+            'type': 'question',
+        },
+    ],
+    'instrument': {
+        'version': '1.0',
+        'id': 'urn:some-test',
+    },
+    'defaultLocalization': 'en',
+}
+
+
+EXPECTED = """{
+  "instrument": {
+    "id": "urn:some-test",
+    "version": "1.0"
+  },
+  "defaultLocalization": "en",
+  "defaultTimeout": {
+    "warn": {
+      "threshold": 40,
+      "text": {
+        "en": "Warning!",
+        "fr": "French warning"
+      }
+    },
+    "abort": {
+      "threshold": 123,
+      "text": {
+        "en": "Abort!"
+      }
+    }
+  },
+  "steps": [
+    {
+      "type": "text",
+      "options": {
+        "text": {
+          "en": "Introductory message"
+        }
+      }
+    },
+    {
+      "type": "question",
+      "options": {
+        "fieldId": "field1",
+        "text": {
+          "en": "Text for field1"
+        },
+        "error": {
+          "en": "You failed"
+        },
+        "enumerations": [
+          {
+            "id": "foo",
+            "text": {
+              "en": "Foo"
+            }
+          },
+          {
+            "id": "bar",
+            "text": {
+              "ar": "Arabic Bar",
+              "en": "Bar"
+            }
+          }
+        ]
+      }
+    }
+  ]
+}"""
+
+
+def test_output_dict():
+    actual = get_interaction_json(INTERACTION, pretty=True)
+    assert actual == EXPECTED, actual
+
--- tests/output_tests.py
+++ tests/output_tests.py
@@ -29,6 +29,24 @@ def test_instrument_output():
         yield check_instrument_output, instrument
 
 
+CALCULATION_FILE = os.path.join(os.path.dirname(__file__), 'examples/calculationsets/good/text.json')
+
+CALCULATION_TESTS = (
+    open(CALCULATION_FILE, 'r'),
+    open(CALCULATION_FILE, 'r').read(),
+    json.load(open(CALCULATION_FILE, 'r')),
+)
+
+def check_calculationset_output(calculationset):
+    output = get_calculationset_json(calculationset)
+    assert isinstance(output, six.string_types)
+    assert len(output) > 0
+
+def test_calculationset_output():
+    for calculationset in CALCULATION_TESTS:
+        yield check_calculationset_output, calculationset
+
+
 ASSESSMENT_FILE = os.path.join(os.path.dirname(__file__), 'examples/assessments/good/text.json')
 
 ASSESSMENT_TESTS = (
@@ -45,4 +63,40 @@ def check_assessment_output(assessment):
 def test_assessment_output():
     for assessment in ASSESSMENT_TESTS:
         yield check_assessment_output, assessment
+
+
+FORM_FILE = os.path.join(os.path.dirname(__file__), 'examples/forms/good/text.json')
+
+FORM_TESTS = (
+    open(FORM_FILE, 'r'),
+    open(FORM_FILE, 'r').read(),
+    json.load(open(FORM_FILE, 'r')),
+)
+
+def check_form_output(form):
+    output = get_form_json(form)
+    assert isinstance(output, six.string_types)
+    assert len(output) > 0
+
+def test_form_output():
+    for form in FORM_TESTS:
+        yield check_form_output, form
+
+
+INTERACTION_FILE = os.path.join(os.path.dirname(__file__), 'examples/interactions/good/text.json')
+
+INTERACTION_TESTS = (
+    open(INTERACTION_FILE, 'r'),
+    open(INTERACTION_FILE, 'r').read(),
+    json.load(open(INTERACTION_FILE, 'r')),
+)
+
+def check_interaction_output(interaction):
+    output = get_interaction_json(interaction)
+    assert isinstance(output, six.string_types)
+    assert len(output) > 0
+
+def test_interaction_output():
+    for interaction in INTERACTION_TESTS:
+        yield check_interaction_output, interaction
 
